891b312ffc72832357cbe53fbece6b87114a9b95
Kanathip <kanathipzaza888@gmail.com>
2021-02-19 13:18:07 +0700

    InsertCourse

---
+++ routes/WlsRouters/InsertCourseRouter.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+
+const insertcourse = require('../../controllers/WlsController/InsertCourseController');
+/* test get username password by ID */
+
+router.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+router.use(bodyParser.json());
+
+router.post("/insertcourse", insertcourse.InsertCourseCSV);
