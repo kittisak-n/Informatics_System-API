df3bf63ae89ab352c791cacd2f6590f6a99f4b8f
stringfly-dev <stringflydev@gmail.com>
2021-03-07 14:06:55 +0700

    push update

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const MpsRouters = require('./routes/MpsRouters/user');
-
-
-
 const WlsRouters = require('./routes/WlsRouters/criteria');
+const Wlscalculate = require('./routes/WlsRouters/Calculation');
+const Wlssummary = require('./routes/WlsRouters/Summary');
 
 var app = express();
 
@@ -21,5 +20,7 @@ app.use(function(req, res, next) {
 
 app.use('/MpsRouters', MpsRouters);
 app.use('/WlsTouters', WlsRouters);
+app.use('/Wlscalculate', Wlscalculate);
+app.use('/Wlssummary', Wlssummary);
 
 app.listen(8080, console.log("API RUNNING PORT 8080"));
