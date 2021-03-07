d3deb5bb00d153d0b38f208da2e0799623c8972f
stringfly-dev <stringflydev@gmail.com>
2021-03-07 14:24:40 +0700

    update

--- routes/WlsRouters/summary.js
+++ routes/WlsRouters/summary.js
@@ -3,24 +3,21 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 
 const router = express.Router();
-
-const summary = require("../../controllers/WlsController/summaryController");
-
-router.use(
-  session({
-    secret: "secret",
+const summary = require('../../controllers/WlsController/summaryController');
+module.exports = router;
+router.use(session({
+    secret: 'secret',
     resave: true,
-    saveUninitialized: true,
-  })
-);
+    saveUninitialized: true
+}));
 
-router.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 router.use(bodyParser.json());
 
-router.post("/get_summary", summary.get_summary);
+router.post("/Get_detail_summary", summary.Get_detail_summary);
+router.post("/Get_person_by_id", summary.Get_person_by_id);
 
-module.exports = router;
+
+
